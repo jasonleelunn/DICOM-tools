bf675f686c99aabc0d5008a46848c272430aa4db
Jason <jasonlunn@live.co.uk>
2020-12-03 16:27:10 +0000

    ADD: dicom-edit jar, working prearchive copying and anonymisation

--- xnat_prearchive_extractor.py
+++ xnat_prearchive_extractor.py
@@ -2,11 +2,18 @@
 
 # Author: Jason Lunn, The Institute of Cancer Research, UK
 
-# Script takes text file with XNAT session names (each on new line) and checks to see if they appear in a
-# project specific region of the prearchive, and returns a text file with all of the timestamps of the matches
+# Script takes csv file with XNAT session names and anonymous patient ID's (each on new line),
+# checks to see if they appear in a project specific region of the prearchive, and can then copy and anonymise
+# the files found
 
+import os
+import shutil
+import subprocess
+import datetime
+import csv
+import re
 import requests
-from tkinter.filedialog import asksaveasfilename, askopenfilename
+from tkinter.filedialog import askopenfilename
 
 
 def get_login_details():
@@ -30,33 +37,88 @@ def get_xnat_session_list(domain, user, pw, project):
 
 def read_session_file():
     filename = askopenfilename(title="Select an Input File")
-    with open(filename) as file:
-        wanted_list = [line.rstrip() for line in file]
-
+    with open(filename, 'r') as file:
+        wanted_list = dict(csv.reader(file, delimiter=','))
+        # wanted_list = [line.rstrip() for line in file]
     return wanted_list
 
 
-# can't seem to download data directly from prearchive with REST, at least for ROI Collections :(
-def get_session(domain, user, pw):
-    download = requests.get(f'{domain}/data/prearchive/projects/Unassigned/20201127_170233013',
-                            auth=(user, pw), verify=False)
-    print(download.status_code, download.text)
+def copy_data(prearchive_path, info_dict, project_id):
+    script_path = askopenfilename(title="Choose an anonymisation profile")
+    print(info_dict)
+    for anon_id, timestamp in info_dict.items():
+        now = str(datetime.datetime.now())[:19]
+        now = now.replace(":", "_")
+        now = now.replace(" ", "_")
+        new_location = f"extracted/{anon_id}_" + str(now) + ".dcm"
+        # for root, dirs, files in os.walk(prearchive_path + timestamp):
+        for root, dirs, files in os.walk(prearchive_path):
+            for file in files:
+                if file.endswith(".dcm"):
+                    filepath = os.path.join(root, file)
+                    shutil.copy(filepath, new_location)
+                    anonymisation(script_path, new_location, anon_id, project_id)
 
-    file_path = "test_download.zip"
-    with open(f"{file_path}", 'wb') as file:
-        file.write(download.content)
+
+def file_info(timestamps_dict):
+    num_files = len(timestamps_dict)
+    print("Total number of files found in prearchive: ", num_files)
+    bytes_total = 0
+    pre_path = input("Absolute path to prearchive data: ")
+    for folder in timestamps_dict.values():
+        for root, dirs, files in os.walk(pre_path + folder):
+            for file in files:
+                if file.endswith(".dcm"):
+                    filepath = os.path.join(root, file)
+                    bytes_total += os.stat(filepath).st_size
+    print(f"Total size of data found: {bytes_total/1000**3}GB")
+    return pre_path
 
 
-def save_output(lines):
-    filename = asksaveasfilename(defaultextension=".txt")
-    if not filename:
-        print("File creation cancelled...")
+def anon_insertion(anon_name, project_id, script_path):
+    # (0010,0010) DICOM tag
+    new_name = f"\n(0010,0010) := \"{anon_name}\" // Anonymised Patient Name\n"
+    # (0010,0020) DICOM tag
+    # new_id = f"\n(0010,0020) := \"{anon_id}\" // Anonymised Patient ID\n"
+
+    # study_desc = f"\n(0008,1030) := \"{project_id}\" // Study Description\n"
+    project_line = f"\nproject := \"{project_id}\"\n"
+    session = "\nsession := format[\"{0}_{1}{2}\", scanDate2, scanTime2, scannerName2]\n"
+    patient_comments = f"\n(0010,4000) := format[\"Project: {project_id}; " + \
+                       f"Subject: {anon_name}; " \
+                       "Session: {0}; " + \
+                       f"AA:True\", session] // Patient Comments\n"
+    # "(0008,0020), substring[(0008,0030), 0, 6], (0008,1090) // Patient Comments"
+
+    with open(script_path, 'r') as standard_script:
+        content = standard_script.read()
+
+    custom_script_path = "customised_script.das"
+    with open(custom_script_path, 'w') as custom_script:
+        custom_script.write(content)
+        custom_script.write(new_name)
+        # custom_script.write(new_id)
+        custom_script.write(project_line)
+        custom_script.write(session)
+        # custom_script.write(study_desc)
+        custom_script.write(patient_comments)
+
+    return custom_script_path
+
+
+def anonymisation(script_path, file_path, anon_id, project_id):
+    custom_script_path = anon_insertion(anon_id, project_id, script_path)
+
+    jar_path = "dicom-edit.jar"
+    run_jar = f"java -jar {jar_path}"
+
+    command = f"{run_jar} -s {custom_script_path} -i {file_path} -o {file_path}"
+    anon = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    jar_error = anon.stderr.read().decode('utf-8')
+
+    if re.search(r"\b" + re.escape('error') + r"\b", jar_error, flags=re.IGNORECASE):
+        print("Error in anonymisation;", jar_error)
         raise SystemExit
-    else:
-        with open(filename, 'w', newline='') as file:
-            for item in lines:
-                file.write("%s\n" % item)
-        print(f"File Created!\nFile is located at '{filename}'")
 
 
 def main():
@@ -65,12 +127,40 @@ def main():
     dom, u, pw = get_login_details()
     wanted_list = read_session_file()
     prearchive_dictionary = get_xnat_session_list(dom, u, pw, project)
+    # print("session: timestamp", prearchive_dictionary)
+    # print("session: anon_id", wanted_list)
 
-    matched_keys = prearchive_dictionary.keys() & wanted_list
-    result = [prearchive_dictionary[k] for k in matched_keys]
+    matched_dict = {wanted_list[k]: prearchive_dictionary[k] for k in prearchive_dictionary.keys() & wanted_list.keys()}
 
-    save_output(result)
+    prearchive_path = file_info(matched_dict)
+
+    continue_check = input("Continue? [y/N]: ")
+    if continue_check == "y":
+        copy_data(prearchive_path, matched_dict, project)
 
 
 if __name__ == "__main__":
     main()
+
+
+# # can't seem to download data directly from prearchive with REST, at least for ROI Collections :(
+# def get_session(domain, user, pw):
+#     download = requests.get(f'{domain}/data/prearchive/projects/Unassigned/20201127_170233013',
+#                             auth=(user, pw), verify=False)
+#     print(download.status_code, download.text)
+#
+#     file_path = "test_download.zip"
+#     with open(f"{file_path}", 'wb') as file:
+#         file.write(download.content)
+
+
+# def save_output(lines):
+#     filename = asksaveasfilename(defaultextension=".txt")
+#     if not filename:
+#         print("File creation cancelled...")
+#         raise SystemExit
+#     else:
+#         with open(filename, 'w', newline='') as file:
+#             for item in lines:
+#                 file.write("%s\n" % item)
+#         print(f"File Created!\nFile is located at '{filename}'")
--- xnat_session_extrapolator.py
+++ xnat_session_extrapolator.py
@@ -8,21 +8,16 @@ from tkinter.filedialog import asksaveasfilename
 
 def get_login_details():
 
-    # domain = input(f"Enter XNAT Domain: ")
-    # username = input(f"Enter Username for {domain}: ")
-    # password = input(f"Enter Password for {username}@{domain}: ")
-
-    domain = 'http://xnatdev.xnat.org'
-    username = 'admin'
-    password = 'TCIABigMemes'
+    domain = input(f"Enter XNAT Domain: ")
+    username = input(f"Enter Username for {domain}: ")
+    password = input(f"Enter Password for {username}@{domain}: ")
 
     return domain, username, password
 
 
 def get_patient_list(domain, user, pw):
 
-    # project = input(f"Enter Project ID: ")
-    project = "TEST_UPLOAD"
+    project = input(f"Enter Project ID: ")
     patients = requests.get(f'{domain}/data/projects/{project}/subjects',
                             auth=(user, pw), verify=False)
     patient_json = patients.json()
@@ -32,15 +27,26 @@ def get_patient_list(domain, user, pw):
     return project, patient_label_list
 
 
+def session_edit(sessions):
+    mod_sessions = []
+    scanner_name = input("Enter scanner name: ")
+    for session in sessions:
+        mod_session = session[0][:16] + scanner_name
+        mod_sessions.append((mod_session, session[1]))
+
+    return mod_sessions
+
+
 def save_output(lines):
-    filename = asksaveasfilename(defaultextension=".txt")
+    filename = asksaveasfilename(defaultextension=".csv")
     if not filename:
         print("File creation cancelled...")
         raise SystemExit
     else:
         with open(filename, 'w', newline='') as file:
             for item in lines:
-                file.write("%s\n" % item)
+                # file.write(f"{item[0], item[1]}\n")
+                file.write(','.join(str(x) for x in item) + '\n')
         print(f"File Created!\nFile is located at '{filename}'")
 
 
@@ -48,15 +54,17 @@ def main():
     dom, u, pw = get_login_details()
     project, patient_list = get_patient_list(dom, u, pw)
 
-    sessions_label_list = []
+    sessions_label_dic = []
     for patient in patient_list:
         sessions = requests.get(f'{dom}/data/projects/{project}'
                                 f'/subjects/{patient}/experiments',
                                 auth=(u, pw), verify=not False)
         sessions_json = sessions.json()
-        [sessions_label_list.append(sesh['label']) for sesh in sessions_json['ResultSet']['Result']]
+        [sessions_label_dic.append((sesh['label'], patient)) for sesh in sessions_json['ResultSet']['Result']]
 
-    save_output(sessions_label_list)
+    edited_dic = session_edit(sessions_label_dic)
+    print(f"Number of sessions found: {len(edited_dic)}")
+    save_output(edited_dic)
 
 
 if __name__ == "__main__":
