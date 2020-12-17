43e8deac59c58fb393a382081a8dc7234f1cf4ab
Jason <jasonlunn@live.co.uk>
2020-12-17 12:32:37 +0000

    ADD: more sophisticated rtss analyser

--- rtss_analyser.py
+++ rtss_analyser.py
@@ -1,17 +1,29 @@
+import os
 import pydicom
 from tkinter.filedialog import askopenfilename
 
-input_file = askopenfilename(title="Select an RTSTRUCT file to analyse")
-# input_file ="rtss_test/DICOM/anon_rtss.dcm"
-ds = pydicom.read_file(input_file, force=True)
+# input_file = askopenfilename(title="Select an RTSTRUCT file to analyse")
+# # input_file ="rtss_test/DICOM/anon_rtss.dcm"
+# ds = pydicom.read_file(input_file, force=True)
 
 # print(ds.dir("contour"))
 # print(ds.roiContourSequence[0])
 # print(ds)
 
 
-def get_roi_labels(file):
+def find_file(anon_id):
+    filepaths = []
+    for root, dirs, files in os.walk(rtss_folder):
+        for file in files:
+            if file.endswith(".dcm") and anon_id in file:
+                filepaths.append(os.path.join(root, file))
+
+    return filepaths
+
+
+def get_roi_labels(input_file):
     labels = []
+    file = pydicom.read_file(input_file, force=True)
 
     if 'RTSTRUCT' in file.Modality:
         sequences = file.StructureSetROISequence
@@ -21,10 +33,17 @@ def get_roi_labels(file):
             labels.append(label)
     else:
         msg = "The file object is not recognised as being RTS or SEG."
-        raise Exception(msg)
+        # raise Exception(msg)
 
     return labels
 
 
-label_list = get_roi_labels(ds)
-print(label_list)
+rtss_folder = "rtss_test"
+patient_num = input("Enter anonymous ID number: ")
+patient_id = f"RS-5293-{patient_num}"
+files = find_file(patient_id)
+
+for filepath in files:
+    label_list = get_roi_labels(filepath)
+    if label_list:
+        print(f"\n{label_list}")
