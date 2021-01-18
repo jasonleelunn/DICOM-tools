fcbc08e9ba87e19a5e204b545be1fae15e06eb03
Jason SurfacePro <jasonlunn@live.co.uk>
2021-01-18 11:10:30 +0000

    ADD: delay to upload per scan on project mirror to attempt prevention of http 104 server response

--- project_mirror.py
+++ project_mirror.py
@@ -12,6 +12,7 @@ from pathlib import Path
 import requests
 import xnat
 from progress.bar import Bar
+import time
 
 
 @contextmanager
@@ -72,6 +73,7 @@ def download(directory, xnat_scans, scan):
 
 
 def upload(domain, directory):
+    time.sleep(2)
     dest_session.post(f'{domain}/data/services/import?inbody=true&import-handler=DICOM-zip'
                       f'&dest=/archive', data=open(f"{directory}.zip", 'rb'))
 
