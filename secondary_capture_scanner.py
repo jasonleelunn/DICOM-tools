ecc690ba4cac96f732de6df32311df5d0059ed12
Jason <jasonlunn@live.co.uk>
2021-02-12 16:07:07 +0000

    ADD: tag (0028,0301) checking with burnt-in PHI image scan checking script

--- secondary_capture_scanner.py
+++ secondary_capture_scanner.py
@@ -2,8 +2,15 @@
 
 # Author: Jason Lunn, The Institute of Cancer Research, UK
 
+"""
+Script to analyse an entire XNAT archive for Secondary Capture and Presentation State Modalities, and scan sets which
+may contain burnt-in PHI in the image pixel data.
+"""
+
+import re
 import csv
 import datetime
+import pydicom.errors
 from pydicom.uid import UID
 import xnat
 import requests
@@ -39,16 +46,24 @@ class FancyBar(ChargingBar):
 class App:
     def __init__(self):
         self.report_table_list = []
+        self.log_list = []
+
+        # SOPClassUIDs to compare metadata against
+        self.secondary_capture_uid = "1.2.840.10008.5.1.4.1.1.7"
+        self.presentation_state_uid = "1.2.840.10008.5.1.4.1.1.11"
+        # self.ct_uid = "1.2.840.10008.5.1.4.1.1.1"
+        # self.mr_uid = "1.2.840.10008.5.1.4.1.1.4"
 
     def xnat_loop(self):
-        # source_dom, source_u, source_pw = user_pass_data("Source")
-        source_dom = "http://xnatdev.xnat.org"
-        source_u = "admin"
-        source_pw = "TCIABigMemes"
+        source_dom, source_u, source_pw = user_pass_data("Source")
+        # source_dom = "http://xnatdev.xnat.org"
+        # source_u = "admin"
+        # source_pw = ""
 
         with xnat.connect(source_dom, user=source_u, password=source_pw) as connection:
             xnat_projects = connection.projects
             print(f"Found {len(xnat_projects)} projects in XNAT database.\n")
+
             with FancyBar('Analysing Database', max=len(xnat_projects)) as bar:
                 for project in xnat_projects:
                     xnat_subjects = xnat_projects[project].subjects
@@ -61,45 +76,81 @@ class App:
                                 self.sop_class_uid_check(scan_data)
                     bar.next()
 
+        date, time_slice = get_date_time()
         if self.report_table_list:
-            self.write_report()
+            self.write_report(date, time_slice)
+        if self.log_list:
+            self.write_log(date, time_slice)
 
     def sop_class_uid_check(self, scan_data):
+        try:
+            dicom_header = scan_data.read_dicom()
+        except ValueError as e:
+            # print(f"Scan {scan_data.id} in session {scan_data.image_session_id} is not a DICOM resource")
+            self.non_dicom_log(scan_data, e)
+        except pydicom.errors.InvalidDicomError as e:
+            self.non_dicom_log(scan_data, e)
 
-        dicom_header = scan_data.read_dicom()
-        sop_class_uid = dicom_header['00080016'].value
-        uid_info = UID(sop_class_uid)
-        # print(uid_info.name)
+        else:
+            sop_class_uid_tag = dicom_header['00080016'].value
+            uid_info = UID(sop_class_uid_tag)
+            burned_in_annotation_tag = "#TAG-NOT-FOUND#"
+            try:
+                burned_in_annotation_tag = dicom_header['00280301'].value
+            except KeyError:
+                pass
 
-        secondary_capture_uid = "1.2.840.10008.5.1.4.1.1.7"
-        # ct_uid = "1.2.840.10008.5.1.4.1.1.1"
-        # mr_uid = "1.2.840.10008.5.1.4.1.1.4"
+            burnt_tag_check = re.search(re.escape("YES"), burned_in_annotation_tag, flags=re.IGNORECASE)
+            if self.secondary_capture_uid in uid_info or self.presentation_state_uid in uid_info or burnt_tag_check:
+                self.scan_location_info(scan_data, uid_info, burned_in_annotation_tag)
 
-        if secondary_capture_uid in uid_info:
-        # if mr_uid in uid_info:
-            # print("Secondary Capture Detected")
-            self.scan_location_info(scan_data)
-
-    def scan_location_info(self, scan_data):
+    def scan_location_info(self, scan_data, uid_info, burned_tag_contents):
         project = scan_data.project
         session_id = scan_data.image_session_id
         scan_name = scan_data.id
-        line = [project, session_id, scan_name]
+
+        uid_name = uid_info.name
+
+        line = [project, session_id, scan_name, uid_name, burned_tag_contents]
         self.report_table_list.append(line)
 
-    def write_report(self):
-        now = str(datetime.datetime.now())
-        date = now[:10].replace("-", "")
-        time = now[11:19].replace(":", "")
+    def write_report(self, date, time_slice):
+        filename = f"{date}_{time_slice}_secondary_capture_report.csv"
+        header = ["Project ID", " Session ID", " Scan ID", " SOPClassUID Name", " Tag (0028:0301) Contents"]
 
-        filename = f"{date}_{time}_secondary_capture_report.csv"
-        header = ["Project ID", "Session ID", "Scan ID"]
-
-        with open(f"logs/{filename}", "wb") as csv_file:
+        with open(f"logs/{filename}", "w") as csv_file:
             writer = csv.writer(csv_file, delimiter=',')
             writer.writerow(header)
+            writer.writerow([])
             for line in self.report_table_list:
                 writer.writerow(line)
+
+    def non_dicom_log(self, scan_data, error_details):
+        project = scan_data.project
+        session_id = scan_data.image_session_id
+        scan_name = scan_data.id
+
+        line = [project, session_id, scan_name, error_details]
+        self.log_list.append(line)
+
+    def write_log(self, date, time_slice):
+        filename = f"{date}_{time_slice}_secondary_capture_log.csv"
+        header = ["Project ID", " Session ID", " Scan ID", " Error Details"]
+
+        with open(f"logs/{filename}", "w") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            writer.writerow(header)
+            writer.writerow([])
+            for line in self.log_list:
+                writer.writerow(line)
+
+
+def get_date_time():
+    now = str(datetime.datetime.now())
+    date = now[:10].replace("-", "")
+    time_slice = now[11:19].replace(":", "")
+
+    return date, time_slice
 
 
 def main():
@@ -108,9 +159,9 @@ def main():
     start = time.time()
     app.xnat_loop()
     end = time.time()
-    elapsed = end - start
-    print(f"Analysis Complete in {elapsed} seconds.\n")
-    print(f"Found {len(app.report_table_list)} secondary capture modality scan sets\n")
+    elapsed = round(end - start, 1)
+    print(f"\nAnalysis Complete in {elapsed} seconds")
+    print(f"Found {len(app.report_table_list)} potential burnt-in PHI scan sets, see readout in logs/ for details\n")
 
 
 if __name__ == "__main__":
