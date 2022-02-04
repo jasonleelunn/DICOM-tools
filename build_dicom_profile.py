68d2cd0830084a77019c8e5fd620dfefe5b8ae4e
Jason Lunn <jasonlunn@live.co.uk>
2022-02-04 10:21:13 +0000

    ADD: more web scraping functionality for DICOM Confidentiality profiles, and script to build .das scripts from downloaded JSON DICOM info

---
+++ build_dicom_profile.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+"""
+Script to create DicomEdit version 6+ conformant .das files from a JSON containing information from Table E1.1 in
+PS3.15 of the DICOM Standard, Application Confidentiality Profile Attributes
+
+Author: Jason Lunn, The Institute of Cancer Research, UK
+"""
+
+import datetime
+import json
+
+"""
+De-identification Action Codes (The DICOM Standard PS3.15 Table E.1-1a.)
+
+D - replace with a non-zero length value that may be a dummy value and consistent with the VR
+Z - replace with a zero length value, or a non-zero length value that may be a dummy value and consistent with the VR
+X - remove
+K - keep (unchanged for non-sequence attributes, cleaned for sequences)
+C - clean, replace with values of similar meaning known not to contain identifying information, consistent with the VR
+U - replace with a non-zero length UID that is internally consistent within a set of Instances
+
+"""
+
+option_descriptions = {'retain_private': "Retain Safe Private Option",
+                       'retain_uids': "Retain UIDs Option",
+                       'retain_device_id': "Retain Device Identity Option",
+                       'retain_institution_id': "Retain Institution Identity Option",
+                       'retain_patient_characteristics': "Retain Patient Characteristics Option",
+                       'retain_full_dates': "Retain Longitudinal Temporal Information with Full Dates Option",
+                       'retain_modified_dates': "Retain Longitudinal Temporal Information with Modified Dates Option",
+                       'clean_descriptions': "Clean Descriptors Option",
+                       'clean_structured_content': "Clean Structured Content Option",
+                       'clean_graphical': "Clean Graphics Option"}
+
+
+def read_json(json_filepath):
+    with open(json_filepath, 'r') as file:
+        json_dict = json.load(file)
+
+    return json_dict
+
+
+def create_profile_header(version, profile_options):
+    today = datetime.date.today()
+    header_text = f"""version \"{version}\"
+                        
+// DICOM Basic Application Level Confidentiality Profile
+// for use with the XNAT Anonymisation Tool
+
+// Created by Jason L. Lunn 
+// Generated on {today.strftime("%d %B %Y")}
+
+
+"""
+    if profile_options:
+
+        options = "\n".join(f"// {option_descriptions[profile]}" for profile in profile_options)
+
+        options_text = f"""// --- Profile Options Enabled ---
+        
+{options}
+
+
+"""
+        header_text = header_text + options_text
+
+    return header_text
+
+
+def create_profile_footer(profile_choices):
+    if 'retain_private' not in profile_choices:
+        remove_private_str = "removeAllPrivateTags"
+    else:
+        remove_private_str = ""
+    footer_text = f"""
+{remove_private_str}
+"""
+
+    return footer_text
+
+
+def build_attribute_modifications(attribute_dict, profile_choices):
+    attribute_output = []
+    for attribute in attribute_dict.values():
+
+        profile_action_code = attribute['basic_profile']
+
+        for profile in profile_choices:
+            # only change code if it is not already set to C (Clean)
+            if profile_action_code != 'C' and attribute[profile]:
+                profile_action_code = attribute[profile]
+
+        modification_str = None
+        if 'D' in profile_action_code:
+            modification_str = f"{attribute['tag_number']} := \"Anon\""
+        elif 'Z' in profile_action_code:
+            modification_str = f"{attribute['tag_number']} := \"\""
+        elif 'X' in profile_action_code:
+            modification_str = f"- {attribute['tag_number']}"
+
+        if modification_str is not None:
+            line = f"{modification_str} // {attribute['attribute_name']}\n"
+            attribute_output.append(line)
+
+    return attribute_output
+
+
+def save_output_file(header="", body=None, footer=""):
+    with open("/Users/jlunn/Desktop/test.das", 'w') as file:
+        file.write(header)
+        file.writelines(body)
+        file.write(footer)
+
+
+def main():
+    filepath = '/Users/jlunn/Desktop/dicom_profiles.json'
+    attribute_dict = read_json(filepath)
+
+    profile_choices = [
+        # 'retain_private',
+        'retain_uids',
+        # 'retain_patient_characteristics',
+        'retain_full_dates',
+        # 'retain_device_id'
+    ]
+
+    dicomedit_version = "6.1"
+
+    head_text = create_profile_header(dicomedit_version, profile_choices)
+    body_text = build_attribute_modifications(attribute_dict, profile_choices)
+    extra_text = create_profile_footer(profile_choices)
+
+    save_output_file(head_text, body_text, extra_text)
+
+
+if __name__ == '__main__':
+    main()
--- web_scraping.py
+++ web_scraping.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Script to scrape relevant DICOM Standard information from HTML tables and store them locally as JSON files
+
+Author: Jason Lunn, The Institute of Cancer Research, UK
+"""
+
 import json
 
 from bs4 import BeautifulSoup
@@ -23,8 +31,52 @@ def standard_sopclassuids():
         json.dump(data_dict, json_file, indent=4)
 
 
+def confidentiality_profiles():
+    url = "https://dicom.nema.org/medical/dicom/current/output/html/part15.html#sect_E.1"
+
+    html = requests.get(url).content
+    soup = BeautifulSoup(html, features="lxml")
+    table_link = soup.find('a', id='table_E.1-1')
+    parent_div = table_link.parent
+    table_body = parent_div.find('tbody')
+
+    attribute_dict = {}
+    table_headers = {0: 'attribute_name',
+                     1: 'tag_number',
+                     2: 'retired',
+                     3: 'in_standard_composite_iod',
+                     4: 'basic_profile',
+                     5: 'retain_private',
+                     6: 'retain_uids',
+                     7: 'retain_device_id',
+                     8: 'retain_institution_id',
+                     9: 'retain_patient_characteristics',
+                     10: 'retain_full_dates',
+                     11: 'retain_modified_dates',
+                     12: 'clean_descriptions',
+                     13: 'clean_structured_content',
+                     14: 'clean_graphical'}
+
+    for row in table_body.find_all('tr'):
+        row_dict = {}
+
+        table_cells = row.find_all('td')
+        for index, label in table_headers.items():
+            data = table_cells[index].text
+            row_dict[label] = data.strip('\n')
+
+            if index == 1:
+                tag_number = data.strip('\n')
+
+        attribute_dict[tag_number] = row_dict
+
+    with open("/Users/jlunn/Desktop/dicom_profiles.json", 'w') as json_file:
+        json.dump(attribute_dict, json_file, indent=4)
+
+
 def main():
-    standard_sopclassuids()
+    # standard_sopclassuids()
+    confidentiality_profiles()
 
 
 if __name__ == '__main__':
